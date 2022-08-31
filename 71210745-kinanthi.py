d63c38a68548d3447da22fac85c81f57933077c6
kinanthi-pangastuti <93354507+kinanthi-pangastuti@users.noreply.github.com>
2022-08-31 18:58:46 +0700

    Add files via upload

---
+++ 71210745-kinanthi.py
@@ -0,0 +1,38 @@
+#Kinanthi P (71210745)
+#UG1-KALKULATOR
+
+print("Silahkan Pilih Menu:")
+print("""1. Penjumlahan
+2. Pengurangan
+3. Perkalian
+4. Pembagian
+Masukkan "Q" untuk keluar.
+=======================""")
+
+while True:
+    x = input("Pilihan: ")
+    if x in "qQ":
+        break
+
+    elif(x == "1"):
+        a = int(input("angka pertama: "))
+        b = int(input("angka kedua: "))
+        hasil = (a + b)
+        print("hasil:", hasil)
+    elif(x == "2"):
+        a = int(input("angka pertama: "))
+        b = int(input("angka kedua: "))
+        hasil = (a - b)
+        print("hasil:", hasil)
+    elif (x == "3"):
+        a = int(input("angka pertama: "))
+        b = int(input("angka kedua: "))
+        hasil = (a * b)
+        print("hasil:", hasil)
+    elif (x == "4"):
+        a = int(input("angka pertama: "))
+        b = int(input("angka kedua: "))
+        hasil = (a / b)
+        print("hasil:", float(hasil))
+
+
